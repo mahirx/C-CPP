2d0cdb7b430af77d4f424c6a317ae8444b9448e0
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-05-05 04:56:22 -0700

    created

---
+++ Chapter11_multidimentional_array/11tablev1.cpp
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	int i, j;
+	int table[10][10];
+
+
+	for (i = 0; i < 20; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			table[i][j]=(i+1)*(j+1);		
+		}
+	}
+
+
+for (i = 0; i < 10; i++)
+	{
+		for (j = 0; j < 10; j++)
+		{
+			printf("%d	", table[i][j]);
+		}
+		printf("\n");
+	}
+
+
+	return 0;
+}
