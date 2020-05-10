a8ff4c21d76cd17f22f542d94374f203f9e3fc9c
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-05-10 03:35:06 -0700

    created

---
+++ Chapter11_multidimentional_array/saarc.cpp
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	char saarc[7][100]={"Bangla", "srilan", "labib", "james", "jack", "india", "nepal"};
+	for (int i = 0; i < 7; i++)
+	{
+		printf("%s\n", saarc[i]);
+	}
+	return 0;
+}
---
+++ Chapter11_multidimentional_array/test.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+
+int main(int argc, char const *argv[])
+{
+	char a[]="mass";
+	printf("%s\n", a);
+	return 0;
+}
