b70f53962abb810ea0b123a2d7ea99df33a9825d
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-05-31 12:21:47 +0600

    Update binary_to_decimal_method2_v1.cpp

--- Chapter12_BinarySearch/binary_to_decimal_method2_v1.cpp
+++ Chapter12_BinarySearch/binary_to_decimal_method2_v1.cpp
@@ -5,14 +5,46 @@
 
 int main(int argc, char const *argv[])
 {
-	int decimal, r, d, i;
+	int decimal, r, d, i, range;
 
 	scanf("%d", &decimal);
 
+	d=decimal;
 
-	for (i = 0; i < d!=0; i++)
-	{
-		
+	for(i=0; pow(2, i) <= decimal; i++){
+		range=i;
 	}
+	// printf("%d\n", range);
+
+
+	char binary[range+1], temp[range+1];
+
+
+	for (i = 0; i <= range; i++)
+	{
+		r=d%2;
+		if (r==0)
+		{
+			binary[i]='0';
+		}
+		else{
+			binary[i]='1';
+		}
+
+		d=d/2;
+	}
+
+	for (i = 0; i <= range ; i++)
+	{
+		temp[i]=binary[i];
+	}
+
+	for (i = 0; i <= range ; i++)
+	{
+		binary[(range-i)]=temp[i];
+	}
+
+
+	printf("%s\n", binary);
 	return 0;
 }
