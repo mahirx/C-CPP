60039b8306dcdd0bdebdff05c4744717dcf18b7e
mahirlabib-inventor <60250437+mahirlabib-inventor@users.noreply.github.com>
2020-07-21 18:41:11 +0600

    Update decending_order.cpp

--- Exercise/Accending_Decending_order/decending_order.cpp
+++ Exercise/Accending_Decending_order/decending_order.cpp
@@ -2,15 +2,29 @@
 
 
 int main(){
-  int ara[5]={1,4,6,8,9};
-  int i;
+  int ara[5]={1,4,6,9,8};
+  int i, temp = 100, j;
+  int ara2[5];
 
+  for (i  = 0; i < 5; i++) {
+    printf("%d\n",ara[i] );
+  }
 
-  // for (i  = 0; i < 5; i++) {
-  //   printf("%d\n",ara[i] );
-  // }
+  for (i = 0; i < 5; i++)
+  {
+  	for (j = i; j < 5; j++)
+  	{
+  		if (ara[j] < temp)
+  		{
+  			temp = ara[j];
+  			ara2[i] = temp; 
+  		}
+  	}
+  }
 
-	  
+	for (i  = 0; i < 5; i++){
+    	printf("%d\n",ara2[i] );
+  	} 
 
   return 0;
 }
